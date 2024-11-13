94a60e7dbd8c73d77d2664519d2f97aea3999da4
skpeterson <skpet@unc.edu>
2024-11-13 13:32:34 -0500

    update script

--- scripts/annotate_DARs.R
+++ scripts/annotate_DARs.R
@@ -18,7 +18,7 @@ degs_all <- read.csv('source_data/TableS3_DEG_between_AD_HC.txt', sep = '\t')
 
 # explore annotating DARs a few different ways ----------------------------
 
-# first, turn DARs into Granges object for subsequent annotation steps 
+# turn DARs into Granges object for subsequent annotation steps 
 
 dars_sitename_splt <- str_split(dars_all$sitename, pattern = '-',simplify = T)
 dars_chr <- dars_sitename_splt[,1]
@@ -28,6 +28,28 @@ dars_end <- as.numeric(dars_sitename_splt[,3])
 dars_gr <- GRanges(seqnames = dars_chr, 
                    ranges = IRanges(start = dars_start, end = dars_end),
                    strand = "*")
+dars_gr$cell_type <- dars_all$cell_type
+
+
+
+# turn DEGs into Granges object for subsequent annotation steps 
+
+# in data as provided, DEGs are just gene_name, but no genomic coordinates
+# annotate with genomic coordinates
+
+# Connect to the Ensembl database
+ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
+
+# Create a vector of feature (gene) names that you want to query
+features <- degs_all$feature
+
+gene_coordinates <- getBM(
+  attributes = c("hgnc_symbol", "chromosome_name", "start_position", "end_position"),
+  filters = "hgnc_symbol",
+  values = features,
+  mart = ensembl
+)
+
 
 ah <- AnnotationHub()
 orgs <- subset(ah, ah$rdataclass == "OrgDb")
