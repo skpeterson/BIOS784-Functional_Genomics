eef21451f70c4fd60eb2dbe7b1037c5359862760
Hazel Milla <155584697+hazelmilla@users.noreply.github.com>
2024-11-18 14:57:57 -0500

    Add files via upload

---
+++ scripts/find_overlaps_distances.R
@@ -0,0 +1,32 @@
+library(GenomicRanges)
+library(GenomicDistributions)
+
+# Make chromosome metadata consistent
+seqlevels(dars_gr) <- gsub("^chr", "", seqlevels(dars_gr))
+
+# Calculate overlaps
+overlaps <- findOverlaps(dars_gr, degs_gr)
+
+# Count peaks that overlap any DEG regions
+dars_near_degs <- length(unique(queryHits(overlaps)))
+dars_near_degs
+
+# calculate distance to nearest - dars & degs
+dist_to_nearest <- distanceToNearest(dars_gr, degs_gr)
+
+# get results
+results <- data.frame(
+  query = queryHits(dist_to_nearest),   # ChIP-seq peak index
+  subject = subjectHits(dist_to_nearest), # Genomic region index
+  distance = mcols(dist_to_nearest)$distance # Distance
+)
+
+glimpse(results)
+
+# Merge metadata with original GRanges
+metadata <- mcols(dist_to_nearest)
+dars_gr_with_metadata <- dars_gr
+mcols(dars_gr_with_metadata) <- cbind(mcols(dars_gr), metadata)
+
+print(dars_gr_with_metadata)
+
