c43a074267e3dfc901bb724cc1a8ff9cf89749df
annikacleven <arcleven@gmail.com>
2024-11-20 14:02:24 -0500

    adding metadata and corresponding df

--- scripts/find_overlaps_distances.R
+++ scripts/find_overlaps_distances.R
@@ -25,6 +25,24 @@ results <- data.frame(
 glimpse(results)
 
 # Merge metadata with original GRanges
+
+# Extract the distances (distances between DARs and nearest DEGs)
+distances <- mcols(dist_to_nearest)$distance
+
+# Extract the indices of the nearest DEGs for each DAR
+nearest_deg_indices <- subjectHits(dist_to_nearest)
+
+# Extract the metadata of the nearest DEGs (metadata from degs_gr)
+nearest_degs_metadata <- degs_gr[nearest_deg_indices]@elementMetadata
+
+# Create a data frame to combine DARs with distances and DEGs metadata
+dars_with_distances_and_metadata <- cbind(as.data.frame(dars_gr), distance = distances, nearest_degs_metadata)
+
+colnames(dars_with_distances_and_metadata) <- c("seqnames", "start", "end", "width", "strand", "cell_type_dar", "distance", "cell_type_deg",
+                                                "gene_name", "gene_biotype", "DAR_deg_overlap")
+
+write_csv(dars_with_distances_and_metadata,"~/BIOS784-Functional_Genomics/working_data/dars_with_distances_and_metadata.csv")
+
 metadata <- mcols(dist_to_nearest)
 dars_gr_with_metadata <- dars_gr
 mcols(dars_gr_with_metadata) <- cbind(mcols(dars_gr), metadata)
