6a9175a67fcd18670c9fbd0621d67991dd5a888a
annikacleven <arcleven@gmail.com>
2024-11-21 20:16:14 -0500

    visualizations

---
+++ various_visualizations.R
@@ -0,0 +1,44 @@
+#visualizing the overlaps 
+
+# check if the the nearest gene is a DEG 
+# dar is harder to determine cell type
+
+
+dars_with_distances_and_metadata %>%
+  ggplot(aes(x = seqnames_dar, y = distance, color = cell_type_dar))+
+  geom_point() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+# log distance by the cell_type (identified by the dar)
+# notes that a differnce in the log at this scale is pretty significant
+dars_with_distances_and_metadata %>%
+  ggplot(aes(x = cell_type_dar, y = log(distance), color = cell_type_dar))+
+  geom_boxplot()
+
+pca <- prcomp(dars_with_distances_and_metadata[, c("distance","logFC_dar")], scale. = TRUE)
+pca_df <- data.frame(pca$x)
+pca_df$cell_type_dar <- dars_with_distances_and_metadata$cell_type_dar
+#pca_df$peak_type <- dars_all$peakType
+
+
+#so much data
+ggplot(pca_df, aes(x = PC1, y = PC2, color = cell_type_dar)) +
+  geom_jitter() +
+  labs(title = "PCA Plot by Cell Type") 
+
+#randomly subset to 1000 
+pca_df %>%
+  sample_n(1000) %>%
+  ggplot(aes(x = PC1, y = PC2, color = cell_type_dar))+
+  geom_jitter() +
+  labs(title = "PCA Plot by Cell Type") 
+#still no real clustering
+
+dars_with_distances_and_metadata %>%
+  ggplot(aes(x = cell_type_deg, y = log(distance), color = cell_type_deg))+
+  geom_boxplot()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+dars_with_distances_and_metadata %>%
+  group_by(cell_type_dar) %>%
+  summarise(mean = mean(distance))
