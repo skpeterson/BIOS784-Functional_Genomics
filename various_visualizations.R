33e1a95107d43d0d6fcddd80a17e028b7a318d25
annikacleven <arcleven@gmail.com>
2024-11-24 15:38:21 -0500

    checking the matching of DAR_deg_overlap var and distance var

--- various_visualizations.R
+++ various_visualizations.R
@@ -1,7 +1,13 @@
 #visualizing the overlaps 
 
-# check if the the nearest gene is a DEG 
-# dar is harder to determine cell type
+# check if the the nearest gene is a DEG
+
+dars_with_distances_and_metadata %>%
+  mutate(is_nearest_deg = gene_name %in% degs_all$feature) %>%
+  group_by(is_nearest_deg) %>%
+  summarise(count_true = sum(is_nearest_deg),
+            count_false = sum(!is_nearest_deg))
+  #NOTE: dar is harder to determine cell type
 
 
 dars_with_distances_and_metadata %>%
@@ -10,7 +16,7 @@ dars_with_distances_and_metadata %>%
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 # log distance by the cell_type (identified by the dar)
-# notes that a differnce in the log at this scale is pretty significant
+# notes that a difference in the log at this scale is pretty significant
 dars_with_distances_and_metadata %>%
   ggplot(aes(x = cell_type_dar, y = log(distance), color = cell_type_dar))+
   geom_boxplot()
@@ -42,3 +48,22 @@ dars_with_distances_and_metadata %>%
 dars_with_distances_and_metadata %>%
   group_by(cell_type_dar) %>%
   summarise(mean = mean(distance))
+
+
+#checking if DAR_deg_overlap matches with our distance
+
+look <- dars_with_distances_and_metadata %>%
+  filter(DAR_deg_overlap == T) %>%
+  select(seqnames_dar, start_dar, end_dar, distance, DAR_deg_overlap)
+
+dars_with_distances_and_metadata %>%
+  filter(DAR_deg_overlap == T) %>%
+  count()
+#there are 169 with overlap
+
+dars_with_distances_and_metadata %>%
+  filter(distance == 0) %>%
+  select(distance, DAR_deg_overlap) %>%
+  table()
+  
+
