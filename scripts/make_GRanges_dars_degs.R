ed96fb685dc4bd9b88367351f00e0fd76135ba9c
skpeterson <skpet@unc.edu>
2024-11-25 15:40:47 -0500

    filter dars and degs by pval

--- scripts/make_GRanges_dars_degs.R
+++ scripts/make_GRanges_dars_degs.R
@@ -16,11 +16,22 @@ suppressPackageStartupMessages({
 dars_all <- read.csv('source_data/TableS2_DAR_between_AD_HC.txt',sep = '\t')
 degs_all <- read.csv('source_data/TableS3_DEG_between_AD_HC.txt', sep = '\t')
 
+## filter data by p-value and log2fc cut offs for significant data 
+dars_filter <- dars_all %>% 
+  filter(LR_padj < 0.05) %>% 
+  filter(avg_log2FC > 0.125 | avg_log2FC < -0.125)
+
+
+degs_filter <- degs_all %>% 
+  filter(MAST_padj < 0.05) %>% 
+  filter(avg_log2FC > 0.125 | avg_log2FC < -0.125)
+
+
 # make DARs Granges object ----------------------------
 
 # turn DARs into Granges object for subsequent annotation steps 
 
-dars_sitename_splt <- str_split(dars_all$sitename, pattern = '-',simplify = T)
+dars_sitename_splt <- str_split(dars_filter$sitename, pattern = '-',simplify = T)
 dars_chr <- dars_sitename_splt[,1]
 dars_start <- as.numeric(dars_sitename_splt[,2])
 dars_end <- as.numeric(dars_sitename_splt[,3])
@@ -28,15 +39,15 @@ dars_end <- as.numeric(dars_sitename_splt[,3])
 dars_gr <- GRanges(seqnames = dars_chr, 
                    ranges = IRanges(start = dars_start, end = dars_end),
                    strand = "*")
-dars_gr$cell_type <- dars_all$cell_type
-dars_gr$logFC_dar <- dars_all$avg_log2FC
+dars_gr$cell_type <- dars_filter$cell_type
+dars_gr$logFC_dar <- dars_filter$avg_log2FC
 
 # make DEGs Granges object ------------------------------------------------
 
 # in data as provided, DEGs are just gene_name, but no genomic coordinates
 # annotate with genomic coordinates
 
-features <- degs_all$feature
+features <- degs_filter$feature
 
 # Load EnsDb and filter gene coordinates
 edb <- EnsDb.Hsapiens.v86
@@ -47,7 +58,7 @@ g <- keepStandardChromosomes(g, pruning.mode = 'coarse') # standard chromosomes
 degs_coords <- g[g$symbol %in% features] %>% as.data.frame()
 
 # Merge DEGs data with gene coordinates so we can have coordinates for our genes
-degs_coords_celltype <- merge(degs_all, degs_coords, by.x = 'feature', by.y = 'symbol')
+degs_coords_celltype <- merge(degs_filter, degs_coords, by.x = 'feature', by.y = 'symbol')
 
 # Filter mapped coordinates by checking that start and end values are not NA
 degs_coords_mapped <- degs_coords_celltype[!is.na(degs_coords_celltype$start) & !is.na(degs_coords_celltype$end), ]
